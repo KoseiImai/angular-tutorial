0f1fb5c3a48b55da2ca6adc4d400bceab5b7d770
KoseiImai <koseiimai2021@gmail.com>
2021-07-04 23:04:03 +0900

    ルーティングの修正

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { HeroesComponent } from './heroes/heroes.component';
 
 const routes: Routes = [
-  { path: 'hero', component: HeroesComponent }
+  { path: 'heroes', component: HeroesComponent }
 ];
 
 @NgModule({
