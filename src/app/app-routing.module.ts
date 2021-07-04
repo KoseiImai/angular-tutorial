f17b86925d374b305943009f5bf4f8ca2e63d3e1
KoseiImai <koseiimai2021@gmail.com>
2021-07-04 23:18:38 +0900

    ダッシュボードの追加

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashbordComponent } from './dashbord/dashbord.component';
 import { HeroesComponent } from './heroes/heroes.component';
 
 const routes: Routes = [
-  { path: 'heroes', component: HeroesComponent }
+  { path: 'heroes', component: HeroesComponent },
+  { path: 'dashbord', component: DashbordComponent },
+  { path: '', redirectTo:'/dashbord', pathMatch: 'full' }
 ];
 
 @NgModule({
