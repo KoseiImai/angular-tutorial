30e0578f6358f46aff603cb73b6c240ac55f3ae5
KoseiImai <koseiimai2021@gmail.com>
2021-07-09 23:13:33 +0900

    idでのデータ取得

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -14,31 +14,44 @@ export class HeroService {
   constructor(
     private messageService: MessageService,
     private http: HttpClient,
-    ) { }
+  ) { }
 
-    private heroesUrl = 'api/heroes';  // Web APIのURL
+  private heroesUrl = 'api/heroes';  // Web APIのURL
 
-    private handleError<T>(operation = 'operation', result?: T) {
-      return (error: any): Observable<T> => {
-        console.error(error);
-        this.log(`${operation} failed: ${error.message}`);
-        return of(result as T);
-      };
-    }
 
+  private log(message: string) {
+    this.messageService.add(`HeroService: $(message)`);
+  }
+    
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // リモート上のロギング基盤にエラーを送信する
+      console.error(error);
+
+      // ユーザーへの開示のためエラーの変数処理を改善する
+      this.log(`${operation} failed: ${error.message}`);
+
+      // からの結果を返して、アプリを持続可能にする
+      return of(result as T);
+    };
+  }
+
+  /** サーバーからデータを取得する */
   getHeroes(): Observable<Hero[]> {
     return this.http.get<Hero[]>(this.heroesUrl).pipe(
+      tap(heroes => this.log('fetched heroes')),
       catchError(this.handleError<Hero[]>('getHeroes', []))
     );
   }
 
+  /** IDよりデータを取得する。見つからなかった場合は404を返却する */
   getHero(id: number): Observable<Hero> {
-    const hero = HEROES.find(h => h.id === id)!;
-    this.messageService.add(`HeroService: fetch hero id=${id}`);
-    return of(hero);
-  }
-
-  private log(message: string) {
-    this.messageService.add(`HeroService: $(message)`);
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get<Hero>(url).pipe(
+      tap(_ => this.log(`fetched hero id=${id}`)),
+      catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
   }
 }
